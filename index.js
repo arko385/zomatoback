0f65b0e4827528f9519b6e9982b26f00af41c82a
arko385 <90547765+arko385@users.noreply.github.com>
2024-02-15 11:15:27 +0530

    change the port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose=require("mongoose");
-const port = 3000;
+const port = process.env.PORT||3000;
 const User=require("./model/user");
 
 //const monguri="mongodb://127.0.0.1:27017/mydb";
